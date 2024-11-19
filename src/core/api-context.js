87cd927296f2de11284f2d7cf1668bd11bf1e810
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-11-19 13:15:08 +0330

    Update api-context.js
    add /

--- src/core/api-context.js
+++ src/core/api-context.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const basUrl="https://react-mini-projects-api.classbon.com";
+const basUrl="https://react-mini-projects-api.classbon.com/";
 
 export const Http= axios.create({
     baseURL:basUrl
