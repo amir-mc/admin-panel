bc3f6651ae62aef5129587cdda8cecfcb38d969b
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-11-23 14:19:47 +0330

    Update change-lan.jsx

--- src/component/change-lan.jsx
+++ src/component/change-lan.jsx
@@ -1,34 +1,44 @@
 import { useEffect, useRef, useState } from "react"
+import { useAppContext } from "../contexts/app/app-context";
+import { changeLanguage } from "i18next";
 
  
  const ChangeLanguage = ()=>{
     const [show,setShow] = useState(false);
     const ref = useRef()
 
-    useEffect(()=>{
-        const chekIfclikOutside =e=>{
-            if (show && ref.current && !ref.current.contains(e.target)){
+    const {language,changeLanguage}=useAppContext()
+
+     useEffect(()=>{
+         const chekIfclikOutside =(e)=>{
+             if (show && ref.current && !ref.current.contains(e.target)){
                 setShow(false)
-            }
-        }
-        document.addEventListener('mousedown',chekIfclikOutside)
-        return ()=>{
-            document.removeEventListener('mousedown',chekIfclikOutside)
-        }
-    },[show])
+             }
+         }
+         document.addEventListener('mousedown',chekIfclikOutside)
+         return ()=>{
+             document.removeEventListener('mousedown',chekIfclikOutside)
+         }
+     },[show])
 
     return(
 
         <div className="dropdown"> 
             <a ref={ref} className="nav-flag dropdown-toggle" onClick={() => setShow(true)}>
-            <p>En†</p>
+            <p>lan</p>
             </a>
             <div className={`dropdown-menu dropdwon-menu-end  ${show ? 'show' : undefined}`}>
-                <a className="dropdown-item fw-bolder">
-                   <p>Fa</p>
+                <a className="dropdown-item fw-bolder" onClick={()=>changeLanguage('fa')} >
+                    <span>
+                        <p>fa</p>
+                    </span>
+                   
                 </a>
-                <a className="dropdown-item fw-bolder"> 
-                     <p>En†</p>
+                <a className="dropdown-item fw-bolder" onClick={()=>changeLanguage('en')} > 
+                    <span>
+                          <p>En†</p>
+                    </span>
+                   
                 </a>
             </div>     
              
