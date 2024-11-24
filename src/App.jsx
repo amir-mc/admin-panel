d0dddae4b15a8e4c0932665b3ce8c88194c7ad74
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-11-24 13:03:30 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import "./core/i18n"
+import { useAppContext } from "./contexts/app/app-context";
+import { useEffect } from "react";
 
 
 
-
+ 
 
 function App() {
 
+const {theme}=useAppContext()
+useEffect(()=>{
+  const head=document.head
+  const link=document.createElement('link')
+  link.rel='stylesheet'
+  link.href=`/css/${theme}.css`
+  head.appendChild(link)
 
+})
   return (
     <RouterProvider router={router}></RouterProvider>
   )
