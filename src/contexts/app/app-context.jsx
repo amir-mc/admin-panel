3baf8d3d38796542e5dbc901770f8de1b989b888
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-11-24 13:01:33 +0330

    Update app-context.jsx

--- src/contexts/app/app-context.jsx
+++ src/contexts/app/app-context.jsx
@@ -5,7 +5,9 @@ import { useTranslation } from "react-i18next";
 const Appcontext=createContext();
    
 const inisialState={
-    language: localStorage.getItem('language') || 'fa' 
+    language: localStorage.getItem('language') || 'fa' ,
+
+    theme: localStorage.getItem('theme') || 'dark'
 }
 
 const Appprovider  = ({children}) =>{
@@ -14,14 +16,20 @@ const Appprovider  = ({children}) =>{
     const changeLanguage = (language)=>{
         dispach({type:'CHANGE_LANGUAGE',payload:language})
     }
-  
+    const changetheme =(theme)=>{
+        dispach({type:'CHANGE_THME',payload:theme})
+    }
+    useEffect(()=>{
+        localStorage.setItem('theme',state.theme)
+    },[state.theme])
+
 
     useEffect(()=>{
         i18n.changeLanguage(state.language)
         localStorage.setItem('language',state.language)
     },[state.language])
 
-    return( <Appcontext.Provider value={{...state,changeLanguage}}>
+    return( <Appcontext.Provider value={{...state,changeLanguage,changetheme}}>
         {children}
         </Appcontext.Provider>)
 }
