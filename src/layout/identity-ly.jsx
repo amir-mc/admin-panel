5cdc71b7be2829fa0cd9d214d63062da8b30e02c
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-11-24 13:04:08 +0330

    Update identity-ly.jsx

--- src/layout/identity-ly.jsx
+++ src/layout/identity-ly.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
 import ChangeLanguage from "../component/change-lan"
+import ChangeTheme from "../component/change-thm"
 
 const IdentiyLayout=()=>{
     return(
         <>
         <div className="container">
             <nav className="navbar shadow-sm">
+                   <ChangeTheme/>
                 <ChangeLanguage/>
             </nav>
         <div className="row justify-content-center">
