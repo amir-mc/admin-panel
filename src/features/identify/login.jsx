9df04e10ff62a6b9ee82d35f6dc27631286ae277
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-11-25 12:46:45 +0330

    Update login.jsx

--- src/features/identify/login.jsx
+++ src/features/identify/login.jsx
@@ -1,25 +1,57 @@
+import { useForm } from "react-hook-form"
+import { useTranslation } from "react-i18next"
+import { Http } from "../../core/api-context"
+import { redirect, useSubmit } from "react-router-dom"
+
 const Login = ()=>{
+    const{register,watch,handleSubmit,formState:{errors}}= useForm()
+
+    const {t}=useTranslation() 
+    const subform=useSubmit()
+    const onSubmtt=data=>{subform(data,{method:'post'})}
     return(
         <>
        
-        <div className="col-md-4 bg-dark">
-            <div className="card shadow bg-light">
+        <div className="col-md-4">
+            <div className="card shadow">
                 <div className="card-body">
                     <h3 className="card-title text-center mb-4">Login</h3>
-                    <form action="/submit_login" method="post">
+                    <form onSubmit={handleSubmit(onSubmtt)} >
                         <div className="mb-3">
                             <label  className="form-label">Username</label>
-                            <input type="text" className="form-control" id="username" name="username" required/>
+
+                            <input {...register('mobile',{
+                                required:'inter name !'
+
+                            })} className={`form-control ${errors.mobile && 'is-invalid'}`}  />
+                            {
+                                errors.name && errors.name.type === 'required' &&(
+                                    <p className="text-danger small fw-bolder mt-1">
+                                       {errors.name.message} 
+                                    </p>
+                                )
+                            }
+
+
                         </div>
                         <div className="mb-3">
+
+
+                        <div className="mb-3">
                             <label  className="form-label">Password</label>
-                            <input type="password" className="form-control" id="password" name="password" required/>
+                            <input {...register('password',{
+                                required:'inter pass'
+                            })} type="password" className={`form-control ${errors.password && 'is-invalid'}`} />
+                        </div>
+
+
+                           
                         </div>
                         <button type="submit" className="btn btn-primary w-100">Login</button>
                     </form>
                 </div>
                 <div className="card-footer text-center mt-2">
-                    <small> ,t have an account? <a href="/regester">Sign up</a></small>
+                    <small> ,t have an account? <a href="/regester">{t('register.register')}</a></small>
                 </div>
             </div> 
         </div>
@@ -27,4 +59,16 @@ const Login = ()=>{
         </>
     )
 }
+
+export async function loginaction({request}) {
+    const formData = await request.formData()
+    const data = Object.fromEntries(formData)
+    const response = await Http.post('/Users/login',data)
+    if(response.status===200){
+        localStorage.getItem('token',response?.data.token)
+        return redirect('/')
+    }
+}
+
+
 export default Login
