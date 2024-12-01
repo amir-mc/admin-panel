97301c91d8db0dc1e6bfded97b37bc74a57d7415
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-12-01 14:20:59 +0330

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./core/i18n"
-
+import { Appprovider } from './contexts/app/app-context.jsx'
 
 createRoot(document.getElementById('root')).render(
 <Appprovider>
