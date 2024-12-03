0d49c3752d6373023d86984dbdd73c13b797d2e7
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-12-03 15:08:58 +0330

    Update api-context.js

--- src/core/api-context.js
+++ src/core/api-context.js
@@ -1,7 +1,24 @@
 import axios from "axios"
 
-const basUrl="https://react-mini-projects-api.classbon.com/";
+
+const basUrl="https://react-mini-projects-api.classbon.com";
 
 export const Http= axios.create({
     baseURL:basUrl
 })
+
+ export const httpintersep = axios.create({
+    baseURL:basUrl
+ })
+ httpintersep.interceptors.request.use(
+    async(config)=>{
+        const token=localStorage.getItem('token')
+        if(token){
+            config.headers={
+                Authorization:`Bearer ${token}`
+            }
+            return config ;
+        }
+        (error)=>Promise.reject(error)
+    }
+ )
