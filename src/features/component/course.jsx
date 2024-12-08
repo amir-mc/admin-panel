4831af047e970a79101591df52c49fb30abb02d5
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-12-08 16:25:50 +0330

    Update course.jsx

--- src/features/component/course.jsx
+++ src/features/component/course.jsx
@@ -1,24 +1,23 @@
-const Course = ({title,coverImageUrl,courseLevel,description,duration,numOfReview})=>{
-return(
-    <div className="card">
-        <img className="card-img-top" src={coverImageUrl}/>
-        <div className="card-header px-4 pt-4 pb-0">
-            <div className="badge bg-primary my-2 fw-bolder">
-                {courseLevel}
+
+import Course from "./course"
+
+const CoursesList = ({courses})=>{
+
+    return(
+        <>
+        
+            <div className="row">
+            {
+                courses.map((coures)=> (
+                    <div className="col-3" key={coures.id}>
+                        <Course {...coures}/>
+                    </div>
+                ))
+            }
             </div>
-            <h4 className="mb-0">{title}</h4>
-        </div>
-        <div className="card-body px-4 pt-2">
-            <p className="text-truncate-3">
-                {description}
-            </p>
-        </div>
-        <div className="cart-footer fs-sm d-flex align-item-center fw-bolder text-secondery justify-content-between">
-            <div className="d-flex align-item-center gap-1">
-                {`${duration} hour`}
-            </div>
-        </div>
-    </div>
-)
+           
+        
+        </>
+    )
 }
-export default Course
+export default CoursesList
