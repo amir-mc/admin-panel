930acef87ebcea423371135d78d8b3d98a7dba29
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-12-08 16:28:11 +0330

    Update api-context.js

--- src/core/api-context.js
+++ src/core/api-context.js
@@ -7,28 +7,28 @@ export const Http= axios.create({
     baseURL:basUrl
 })
 
- export const httpintersep = axios.create({
+export const interSeptservise=axios.create({
     baseURL:basUrl
- })
- httpintersep.interceptors.request.use(
+})
+interSeptservise.interceptors.request.use(
     async(config)=>{
-        const token=localStorage.getItem('token')
+        const token = localStorage.getItem('token')
         if(token){
             config.headers={
                 Authorization:`Bearer ${token}`
-            }
-            return config ;
+            };
+
         }
-        (error)=>Promise.reject(error)
-    }
- )
- httpintersep.interceptors.response.use(
+        return config
+    },
+    (error)=>Promise.reject(error)
+)
+interSeptservise.interceptors.response.use(
     (response)=>response,
-        async(error)=>{
-            if(error.response.status===401){
-                window.location.href='/login'
-            }
-            return Promise.reject(error)
+    async(error)=>{
+        if(error.response.status===401){
+            window.location.href = '/login'
         }
-    
- )
+        return Promise.reject(error)
+    }
+)
