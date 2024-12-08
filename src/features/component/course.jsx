400af2d741b98ce7d747026b85414d089272492b
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-12-08 09:51:17 +0330

    Update course.jsx

--- src/features/component/course.jsx
+++ src/features/component/course.jsx
@@ -2,6 +2,22 @@ const Course = ({title,coverImageUrl,courseLevel,description,duration,numOfRevie
 return(
     <div className="card">
         <img className="card-img-top" src={coverImageUrl}/>
+        <div className="card-header px-4 pt-4 pb-0">
+            <div className="badge bg-primary my-2 fw-bolder">
+                {courseLevel}
+            </div>
+            <h4 className="mb-0">{title}</h4>
+        </div>
+        <div className="card-body px-4 pt-2">
+            <p className="text-truncate-3">
+                {description}
+            </p>
+        </div>
+        <div className="cart-footer fs-sm d-flex align-item-center fw-bolder text-secondery justify-content-between">
+            <div className="d-flex align-item-center gap-1">
+                {`${duration} hour`}
+            </div>
+        </div>
     </div>
 )
 }
