b84b166cac83a14f47818b4d813f85ed65c76952
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-12-12 13:43:34 +0330

    Update categorylist.jsx

--- src/features/category/categorylist.jsx
+++ src/features/category/categorylist.jsx
@@ -1,16 +1,48 @@
 import { useLoaderData } from "react-router-dom"
-
+ 
 const CategoryList = ({category:{data,totalRecord}})=>{
 
     return(
         <>
-        {
-            data.map((category)=>(
-                <div key={category.id}>
-                    {category.name}
-                </div>
-            ))
-        }
+       <div className="row">
+        <div className="col-12">
+            <div className="card">
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>
+                                name
+                            </th>
+                            <th>
+                                op
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            data.map((cate)=>{
+                               return(
+                                <tr key={cate.id}>
+                                    <td >
+                                        {cate.name}
+                                    </td>
+                                    <td className="table-action">
+                                        <a>
+
+                                        </a>
+                                    </td>
+
+                                </tr>
+                               )
+
+                            }
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+       </div>
         </>
     )
 }
