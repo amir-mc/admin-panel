aac6c562fcdb9bda9cebace00d635b9c38f8567d
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-12-15 17:25:07 +0330

    Update categoty-cours.jsx

--- src/pages/categoty-cours.jsx
+++ src/pages/categoty-cours.jsx
@@ -1,11 +1,46 @@
-const CategotyCours  =()=>{
+import { Await, defer, useLoaderData } from "react-router-dom"
+import { interSeptservise } from "../core/api-context"
+import { Suspense } from "react"
+import CoursesList from "../features/component/courses-list"
+import CategoryList from "../features/category/categorylist"
 
+const CategotyCours  =()=>{
+    const data = useLoaderData()
     return(
         <>
-        <div>
-            cl500
+            <div className="row">
+        
+        <div className="col-12" >
+            <div className="d-flex align-item-center justify-content-between mb-5">
+                <a className="btn btn-primery">
+                    add category
+                </a>
+            </div>
+            <Suspense fallback={<p>Loading</p>}>
+            <Await resolve={data.category}>
+                {    
+                    (loadCategory)=> <CategoryList category={loadCategory}/>
+                }
+            </Await>
+            </Suspense>
         </div>
+
+         </div>
         </>
     )
+}
+export async function categorycourse({request}) {
+    return defer({
+        category:loadCategory(request)
+    })
+    
+}
+const loadCategory= async (request)=>{
+    const page = new URL(request.url).searchParams.get('page') || 1;
+    const pagenum=2;
+    let url="/CourseCategory/sieve"
+    url +=`?page=${page}&pagesize=${pagenum}`
+    const response = await interSeptservise.get(url)
+    return response.data
 }
 export default CategotyCours
