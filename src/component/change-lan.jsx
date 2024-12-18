47d8d8557073a710c427820d21452b2543750270
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-12-18 17:44:08 +0330

    Update change-lan.jsx
    debug the language button

--- src/component/change-lan.jsx
+++ src/component/change-lan.jsx
@@ -9,40 +9,61 @@ import { changeLanguage } from "i18next";
 
     const {language,changeLanguage}=useAppContext()
 
-     useEffect(()=>{
-         const chekIfclikOutside =(e)=>{
-             if (show && ref.current && !ref.current.contains(e.target)){
-                setShow(false)
-             }
-         }
-         document.addEventListener('mousedown',chekIfclikOutside)
-         return ()=>{
-             document.removeEventListener('mousedown',chekIfclikOutside)
-         }
-     },[show])
+      useEffect(() => {
+        setShow(false);
+      }, [language])
+    
+    
+      useEffect(() => {
+        const checkIfClickOutside = (e) => {
+          if (show && ref.current && !ref.current.contains(e.target)) {
+            setShow(false);
+          }
+        };
+        document.addEventListener("mousedown", checkIfClickOutside);
+
+        return () => {
+          document.removeEventListener("mousedown", checkIfClickOutside);
+        };
+      }, [show]);
+    //  useEffect(()=>{
+    //      const chekIfclikOutside =(e)=>{
+    //          if (show && ref.current && !ref.current.contains(e.target)){
+    //             setShow(false)
+    //          }
+    //      }
+    //      document.addEventListener('mousedown',chekIfclikOutside)
+    //      return ()=>{
+    //          document.removeEventListener('mousedown',chekIfclikOutside)
+    //      }
+    //  },[show])
 
     return(
 
         <div className="dropdown"> 
-            <a ref={ref} className="nav-flag dropdown-toggle" onClick={() => setShow(true)}>
-            <p>lan</p>
-            </a>
-            <div className={`dropdown-menu dropdwon-menu-end  ${show ? 'show' : undefined}`}>
-                <a className="dropdown-item fw-bolder" onClick={()=>changeLanguage('fa')} >
-                    <span>
-                        <p>fa</p>
-                    </span>
-                   
-                </a>
-                <a className="dropdown-item fw-bolder" onClick={()=>changeLanguage('en')} > 
-                    <span>
-                          <p>En†</p>
-                    </span>
-                   
-                </a>
-            </div>     
+            <a className="nav-flag dropdown-toggle" onClick={() => setShow(true)}>
+        <img  alt="English" />
+        fa
+      </a>
+
+      <div
+        ref={ref}
+        className={`dropdown-menu dropdown-menu-end ${
+          show ? "show" : undefined
+        }`}
+      >
+        <a className="dropdown-item fw-bolder d-flex align-items-center gap-2" style={{ textAlign: language === 'fa' ? "right" : 'left'}} onClick={() => changeLanguage('fa')}>
+          <img  width="20" className="ms-2" />
+          <span className="align-middle">فارسی</span>
+        </a>
+        <a className="dropdown-item fw-bolder d-flex align-items-center gap-2"  style={{ textAlign: language === 'fa' ? "right" : 'left'}} onClick={() => changeLanguage('en')}>
+          <img  width="20" className="ms-2" />
+          <span className="align-middle">English</span>
+        </a>
+      </div>
              
         </div>
+        
         
 
     )
