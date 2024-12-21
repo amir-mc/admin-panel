93f0e66276114b4a6d39b685f301be7e5aa71061
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-12-21 14:38:06 +0330

    Update modal.jsx

--- src/component/modal.jsx
+++ src/component/modal.jsx
@@ -1,1 +1,12 @@
 
+import { createPortal } from "react-dom"
+
+const Modal=({isOpen,children,body,close,title})=>{
+    return(
+        <>
+        {
+            isOpen && createPortal(<div></div>,document.getElementById('modal'))
+        }
+        </>
+    )
+}
