09e77aacdf6b005bbff302dfd14c6bea366a0c87
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-12-22 19:24:52 +0330

    Update modal.jsx

--- src/component/modal.jsx
+++ src/component/modal.jsx
@@ -1,12 +1,32 @@
-
+import { useState } from "react"
 import { createPortal } from "react-dom"
 
 const Modal=({isOpen,children,body,close,title})=>{
+
     return(
         <>
         {
-            isOpen && createPortal(<div></div>,document.getElementById('modal'))
+            isOpen && createPortal(<div className="modal" style={{display:"block"}}>
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title fw-bolder">{title}</h5>
+                            <button type="button" className="btn-close m-5" onClick={()=>close(false)}>
+
+                            </button>
+                        </div>
+                        <div className="modal-body m-3">
+                            <p className="mb-0">{body}</p>
+
+                        </div>
+                        <div className="modal-footer">
+                            {children}
+                        </div>
+                    </div>
+                </div>
+            </div>,document.getElementById('modal'))
         }
         </>
     )
 }
+export default Modal
