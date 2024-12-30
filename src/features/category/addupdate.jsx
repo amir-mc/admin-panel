b5a99530198901495509836f95c0f96fcc8c3239
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-12-30 12:06:18 +0330

    Update addupdate.jsx

--- src/features/category/addupdate.jsx
+++ src/features/category/addupdate.jsx
@@ -1,42 +1,65 @@
 import { useForm } from "react-hook-form"
-import { Form, useNavigate } from "react-router-dom"
+import {  useNavigate } from "react-router-dom"
 import { interSeptservise } from "../../core/api-context"
 
 import { toast } from "react-toastify"
+import { useCategoryContext } from "./category-context"
+import { useEffect } from "react"
+
 
 const Addorupdate = ({setaddup})=>{ 
-const {register,handleSubmit,formState:{errors}}= useForm();
+const {register,setValue,handleSubmit,formState:{errors}}= useForm();
 const navigat=useNavigate();
+const {category,setCategory}=useCategoryContext()
 
-    const onSubmit=(data)=>{
-        const response  = interSeptservise.post(`/CourseCategory/`,data);
-        toast.promise(
-                    response,{
-                          pending:'HOLD ON',
-                        success:{
-                            render(){
-                                const url=new URL(window.location.href)
-                                 navigat(url.pathname+url.search)
-                                 return 'DONE'
-                            }
+    useEffect(()=>{ 
+        if(category){
+            setValue('name',category.name),
+            setValue('id',category.id)
+        }
+    },[category])
+    const onClose=()=>{
+        setaddup(false)
+        setCategory(null)
+    }
+   
+    const onSubmit=(data)=>{ 
+         const response  = interSeptservise.post(`/CourseCategory/`,data);
+
+          toast.promise(
+                      response,{
+                            pending:'HOLD ON',
+                          success:{
+                              render(){
+                                  const url=new URL(window.location.href)
+                                   navigat(url.pathname+url.search)
+                                   if(category){
+                                    return 'change DONE'
+                                   }else{
+                                     return 'DONE'
+                                   }
+                                  
+                              }
         
-                        },
-                        error:{render({data}){
-                            if(data.response.status===400){
-                                return(<p>error</p>)
-                            }else{
-                                return 'ERROR iN SRVER'
-                            }
-                        }}
-                    },{
-                        position: "bottom-left"
-                    }
+                    },
+                          error:{render({data}){
+         if(data.response.status===400){
+                                  return(<p>error</p>)
+                         }else{
+                             return 'ERROR iN SRVER'
+                        }
+                   }}
+               },{
+                  position: "bottom-left"
+              }
                 
-                )
-      //  <Tosti response={response} navigat={navigat} />
+           )
+       
+ 
         
     }   
     return(
+        
         <div className="card">
             <div className="caard-body">
                 <form className="mb-4" onSubmit={handleSubmit(onSubmit)}>
@@ -52,9 +75,9 @@ const navigat=useNavigate();
                     }
                     </div>
                     <div className="text-star mt-3">
-                        <button type="button" className="btn btn-secondary ms-2" onClick={()=>setaddup(false)}>close add category </button>
-                        <button type="submit" className="btn btn-primary ms-2" >add category</button>
-                    
+                        <button type="button" className="btn btn-secondary ms-2" onClick={onClose}>close add category </button>
+                        <button type="submit" className="btn btn-primary ms-2" >CHANGE</button>
+                     
                     </div>
 
                 </form>
